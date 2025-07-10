7d2a0e593d5f19ec1dd68c315d868d00ab2f5789
Softcode-learner <njha1836@gmail.com>
2025-07-10 16:47:37 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'XYZ',
+    author_email= 'ABC@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
